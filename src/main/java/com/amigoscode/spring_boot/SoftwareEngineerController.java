a143563f0b59e9dcb101b446d199901a02fb7f82
malik-builds <malikmackey0@outlook.com>
2026-06-15 05:59:37 +0530

    created SWE endpoint to get all SWEs

---
+++ src/main/java/com/amigoscode/spring_boot/SoftwareEngineerController.java
@@ -0,0 +1,29 @@
+package com.amigoscode.spring_boot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
+public class SoftwareEngineerController {
+    @GetMapping
+    public List<SoftwareEngineer> getEngineers(){
+        return List.of(
+                new SoftwareEngineer(
+                        1,
+                        "James",
+                        "JS, Node, React, TailWindCSS"
+                ),
+                new SoftwareEngineer(
+                        2,
+                        "Jamila",
+                        "java, spring boot, react, tailwindcss"
+                )
+
+        );
+    }
+
+}
